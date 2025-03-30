ee4f407901de40c8a67e0015b9053bd713045e0f
niLPotential <niLPotential@proton.me>
2025-03-30 19:35:47 +0900

    Setup content collections

---
+++ src/blog/test.md
@@ -0,0 +1,14 @@
+---
+title: Test page
+description: This is a test page
+pubDate: 2024/02/02
+---
+# Test
+
+Lorem Ipsum
+
+## Testing Typography
+
+**Bold**
+
+*italic*
---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+import { glob, file } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updateDate: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = { blog };
