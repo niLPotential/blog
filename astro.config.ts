7226b5ec5b83a4893ea70031cd35cae289db9471
niLPotential <niLPotential@proton.me>
2025-03-30 22:29:40 +0900

    Add alpinejs

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
 import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    alpinejs(),
     UnoCSS({
       injectReset: true,
     }),
