1d360111a9e822b569418c4716ee0dea3fa318d1
niLPotential <niLPotential@proton.me>
2025-03-30 17:17:38 +0900

    Add @iconify-json/meteor-icons as dep

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,15 @@
-import { defineConfig, presetMini } from "unocss";
+import { defineConfig, presetIcons, presetMini } from "unocss";
 
-export default defineConfig({ presets: [presetMini()] });
+export default defineConfig({
+  presets: [
+    presetIcons({
+      collections: {
+        "meteor-icons": () =>
+          import("@iconify-json/meteor-icons/icons.json").then(
+            (i) => i.default,
+          ),
+      },
+    }),
+    presetMini(),
+  ],
+});
